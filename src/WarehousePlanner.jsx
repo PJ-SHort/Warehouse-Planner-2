c1d2bccdadaabfd5399cf7cdb71efbce37d0bf2b
PJ-SHort <paulshort@hotmail.com>
2025-04-18 16:11:44 -0700

    Update WarehousePlanner.jsx

--- src/WarehousePlanner.jsx
+++ src/WarehousePlanner.jsx
@@ -1,5 +1,10 @@
+// src/WarehousePlanner.jsx
+import React from 'react';
+
 export default function WarehousePlanner() {
-  return <div style={{ padding: 20, fontSize: 24 }}>
-    🚧 Hello from WarehousePlanner!
-  </div>;
+  return (
+    <div style={{ padding: 20, fontSize: 24 }}>
+      🚀 Hello from WarehousePlanner!
+    </div>
+  );
 }
